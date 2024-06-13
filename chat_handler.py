640a88cebc310402b8523c12ebf122d16abd31a1
Caio P. <caio@example.com>
2024-06-13 14:17:32 -0300

    Add ETA detector

--- chat_handler.py
+++ chat_handler.py
@@ -1,6 +1,11 @@
 import discord
 import io
+import providers
 import traceback
+import numpy as np
+import json
+
+from openai import AsyncOpenAI
 from discord.ext import commands
 from vector_db import QdrantVectorDbConnection
 from rate_limits import RateLimit, RateLimiter
@@ -41,8 +46,88 @@ class ChatHandler(commands.Cog):
 
         return True
 
+    async def answer_eta_question_if_needed(self, message: discord.Message) -> bool:
+        MIN_SIMILARITY = 0.5
+        # 'updat' to catch update, updating, etc
+        needed_keywords = ["eta", "when", "out", "will", "paper", ".", "release", "updat", "progress", "come"] 
+        oai_client = AsyncOpenAI(api_key=providers.get_provider_by_name("EMBEDDINGS_PROVIDER").api_key)
+        query = message.content
+
+        if not any([kw in message.content.lower() for kw in needed_keywords]):
+            return False
+
+        query_emb = await oai_client.embeddings.create(
+            model="text-embedding-3-large", 
+            input=message.content.lower()
+        )
+        reference_emb = await oai_client.embeddings.create(
+            model="text-embedding-3-large", 
+            input="Is there any ETA / estimate / progress on when 1.21 will release / come out?"
+        )
+        similarity = np.dot(query_emb.data[0].embedding, reference_emb.data[0].embedding)
+        if similarity < MIN_SIMILARITY:
+            return False
+
+        llm_classification_resp = await oai_client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            temperature=0,
+            response_format={"type": "json_object"},
+            messages=[{
+                "role": "system", 
+                "content": 
+                """
+                You classify queries that may possibly be asking for estimates (ETA - estimated time of arrival) or release dates for Paper 1.21.
+                Given a query, return only a JSON containing :
+                * wants_release_info: If the query aims to get info about the update, true or false
+                * project_name: The name of the project if any, may be "none"
+                * version: The version mentioned if any, may be "none" 
+
+                Examples:
+                Query: hi, do you know when 1.21 will be out?
+                JSON: {"wants_release_info": true, "": project_name: "none", "version": "1.21"}
+                Query: I would like to know when Paper will update
+                JSON: {"wants_release_info": true, "": project_name: "Paper", "version": "none"}
+                Query: vanilla 1.20 out when????
+                JSON: {"wants_release_info": true, "": project_name: "vanilla", "version": "1.20"}
+                Query: I hate it when people keep asking if 1.21 will come out
+                JSON: {"wants_release_info": false, "": project_name: "none", "version": "1.21"}
+                Query: man, i wish Paper would just hard fork so they can update to 1.21
+                JSON: {"wants_release_info": false, "": project_name: "Paper", "version": "1.21"}
+                Query: where is velocity 1.21??
+                JSON: {"wants_release_info": false, "": project_name: "Velocity", "version": "1.21"}
+
+                The query is now ((query)). Reply with just the corresponding JSON.
+                JSON: 
+                """.replace("((query))", query)
+            }]
+        )
+        response_content = llm_classification_resp.choices[0].message.content
+        response_json = json.loads(response_content)
+
+        if not response_json["wants_release_info"]:
+            return False
+
+        proj_name: str = response_json["project_name"]
+        is_third_party_project = not any(proj in proj_name.lower() for proj in ["none", "paper", "velocity"])
+        version = response_json["version"]
+
+        is_eta_question = any([version == "1.21", not is_third_party_project])
+
+        if is_eta_question:
+            await message.reply(
+                embed=discord.Embed(
+                    title="No ETA!", 
+                    description="Paper does not publish ETAs for releases, or estimates based on previous versions. Every version is different! Please stay tuned for new announcements."
+                )
+            )
+
+        return is_eta_question 
+
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message) -> None:
+        if await self.answer_eta_question_if_needed(message):
+            return
+
         if not await self.should_process_message(message):
             return
 
