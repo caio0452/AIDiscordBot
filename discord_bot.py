ec38a2590b06067fe37066af3f8a560d5f3ba749
Caio P. <caio@example.com>
2024-06-13 15:25:47 -0300

    Add message contents intent for bot

--- discord_bot.py
+++ discord_bot.py
@@ -1,4 +1,7 @@
 import discord
 from discord.ext import commands
 
-INSTANCE = commands.Bot(command_prefix='paper!', intents=discord.Intents.default())
+intents = discord.Intents.default()
+intents.message_content = True
+
+INSTANCE = commands.Bot(command_prefix='paper!', intents=intents)
