d02b270ca40a3fc7da0110687ff13ae78d59d98f
Caio P. <caio@example.com>
2024-06-22 16:28:59 -0300

    OpenRouter returns null for 3.5 Sonnet image requests, try the beta one

--- ai.py
+++ ai.py
@@ -76,7 +76,7 @@ class OAICompatibleProvider:
         print(initial_msg)
         raw_response = await self.client.chat.completions.create(
             messages=[initial_msg],
-            model="anthropic/claude-3.5-sonnet",
+            model="anthropic/claude-3.5-sonnet:beta",
             max_tokens=4096
         )
         print(raw_response.choices[0])
