3ce8c35ad85fff90c10dae51d58204f9b89bc5c0
Caio P. <caio@example.com>
2024-07-25 20:10:37 -0300

    Fallback to big llama

--- kami_chan/kami_chan.py
+++ kami_chan/kami_chan.py
@@ -99,7 +99,7 @@ class DiscordBotResponse:
         raise RuntimeError("Could not generate response")
 
     async def create(self, message: discord.Message) -> str:
-        return await self.create_or_fallback(message, ["openai/gpt-4o-mini"])
+        return await self.create_or_fallback(message, ["openai/gpt-4o-mini", "meta-llama/llama-3.1-405b-instruct"])
 
     async def personality_rewrite(self, message: str) -> str:
         response = await self.bot_data.clients[PERSONALITY_REWRITER_NAME].generate_response(
