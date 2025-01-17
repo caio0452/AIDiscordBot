b249e1e4b4266b2fe29c02b047a209355ecd4a7b
Caio <caio@example.com>
2025-01-16 21:03:54 -0300

    Operate on copy of full prompt rather than original object

--- bot_workflow/ai_bot.py
+++ bot_workflow/ai_bot.py
@@ -172,7 +172,7 @@ class DiscordBotResponse:
         user_query = await self.user_query_rephrase()
         knowledge = await self.info_select(user_query)
         old_memories: str = "" # TODO: implement
-        full_prompt: Prompt = self.bot_data.profile.prompts[NAME]
+        full_prompt: Prompt = self.bot_data.profile.prompts[NAME].model_copy()
 
         if knowledge is not None:
             knowledge_str = f"\n[INFO FROM KNOWLEDGE DB]:\n{knowledge}\n"
