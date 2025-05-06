35199fc57a45b5b2765d98379627ca23f73ae924
Caio P. <caio@example.com>
2025-05-05 21:44:56 -0300

    Move basic functionality to core

--- commands/sync_command_tree.py
+++ commands/sync_command_tree.py
@@ -3,18 +3,14 @@ from discord import app_commands
 from discord.ext import commands
 
 class SyncCommand(commands.Cog):
-    def __init__(self, bot: commands.Bot, *, allowed_user_id: int) -> None:
+    def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
-        self.allowed_user_id = allowed_user_id
     
     @app_commands.command(
         name="sync", 
         description="Sync the command tree"
     )
+    @commands.is_owner()
     async def sync_commands(self, interaction: discord.Interaction) -> None:
-        if interaction.user.id != self.allowed_user_id:
-            await interaction.followup.send("You do not have permission to run this command.")
-            return
-        
         await self.bot.tree.sync()
         await interaction.followup.send("Command tree synced.")
